1f1aba8d085a19d9fef5e9735da5635ee6437da0
admin <admin@DESKTOP-V2UD3RC>
2022-12-30 17:33:23 +0900

    정수 3개 삼항연산자로 최대 최소 구하기

---
+++ javaproject/src/p2022_12_29/Pr2.java
@@ -0,0 +1,28 @@
+package p2022_12_29;
+
+import java.util.Scanner;
+
+public class Pr2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n1, n2, n3, max, min, temp, temp2;
+		System.out.println("정수 3개를 입력하세요?");
+		Scanner sc = new Scanner(System.in);		//Scanner 객체 생성
+		n1 = sc.nextInt();	//입력값 스페이스바 or 엔터키로 구분	
+		n2 = sc.nextInt();		
+		n3 = sc.nextInt();		
+		
+		temp = (n1 > n2) ? n1 : n2;
+ 		temp2 = (n1 < n2) ? n1 : n2;
+		
+		max = (n3 > temp)	? n3 : temp;		//최대값
+		min = (n3 < temp2)	? n3 : temp2;		//최소값
+		
+		System.out.println("max:" + max);
+		System.out.println("min:" + min);
+		
+	}
+
+}
