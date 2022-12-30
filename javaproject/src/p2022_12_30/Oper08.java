28a3a3fb8b6cde0a2536e3e5c7a4c00d084ddd0a
admin <admin@DESKTOP-V2UD3RC>
2022-12-30 17:29:09 +0900

    boolean 이용 ! 연산자 학습

---
+++ javaproject/src/p2022_12_30/Oper08.java
@@ -0,0 +1,19 @@
+package p2022_12_30;
+
+public class Oper08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 논리 연산자 : and(&&), or(||), not(!)
+		
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		System.out.println(!b1);		//false
+		System.out.println(!b2);		//true
+		System.out.println(!true);		//false
+		System.out.println(!false);		//true
+		
+	}
+
+}
