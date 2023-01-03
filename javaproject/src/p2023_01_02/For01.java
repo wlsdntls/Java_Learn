78763230f67a7d23a2f6582fd6857e2a1f56da24
admin <admin@DESKTOP-V2UD3RC>
2023-01-03 09:03:51 +0900

    for문 사용 출력 연습

---
+++ javaproject/src/p2023_01_02/For01.java
@@ -0,0 +1,19 @@
+package p2023_01_02;
+
+public class For01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		for문
+//	    for(초기값; 조건식; 증감식){
+//	       반복 실행할 문장;
+//	    }		
+
+		for (int i = 1; i < 11; i++) {
+			System.out.println(i + "사랑해요");
+		}
+		
+	}
+	
+}
